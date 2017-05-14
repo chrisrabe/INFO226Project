32003285f4378884aaea077d820a855576da9f99
Hannah van den Kerkhof <hannahvandenkerkhof@Hannahs-MacBook-Pro-4.local>
2017-05-14 14:29:50 +1200

    Project dummy values

--- testhelper.js
+++ testhelper.js
@@ -19,5 +19,106 @@ var printObject = function (object) {
 };
 
 var getProjects = function () {
-    return [];
+    return [{
+    ProjectID: "1",
+    Name: "Scaffolding and painting.",
+    BuildingID: "123",
+    Status: "done",
+    StartDate: "2016-12-12T00:00:00",
+    EndDate: "2016-12-14T00:00:00",
+    ContactPerson: "Joe Bloggs",
+    ProjectManager: "Sally Smith",
+    Contractor: "ABC Company",
+    Works: [{
+            TypeOfWork: "scaffolding",
+            Status: "done"
+        },
+        {
+            TypeOfWork: "painting",
+            Status: "done"
+        }
+    ],
+
+    Comments: [{
+        Author: "Adele",
+        Text: "Hello."
+    }, {
+        Author: "John Doe",
+        Text: "Problem detected."
+    }]},
+    {
+    ProjectID: "2",
+    Name: "Roofing",
+    BuildingID: "123",
+    Status: "on-going",
+    StartDate: "2016-12-12T00:00:00",
+    EndDate: "2016-12-18T00:00:00",
+    ContactPerson: "Joe Bloggs",
+    ProjectManager: "Sally Smith",
+    Contractor: "ABC Company",
+    Works: [{
+            TypeOfWork: "roofing",
+            Status: "on-going"
+        },
+    ],
+
+    Comments: [{
+        Author: "Adele",
+        Text: "Problem fixed"
+    }, {
+        Author: "John Doe",
+        Text: "Problem detected."
+    }]},
+    {
+    ProjectID: "3",
+    Name: "Tiling & plastering.",
+    BuildingID: "222",
+    Status: "on-going",
+    StartDate: "2016-12-16T00:00:00",
+    EndDate: "2016-12-20T00:00:00",
+    ContactPerson: "Jim Jones",
+    ProjectManager: "Bob Smart",
+    Contractor: "Building Ltd",
+    Works: [{
+            TypeOfWork: "tiling",
+            Status: "done"
+        },
+        {
+            TypeOfWork: "plastering",
+            Status: "on-going"
+        }
+    ],
+
+    Comments: [{
+        Author: "Dave Kerkhof",
+        Text: "Going well."
+    }]},
+    {
+    ProjectID: "4",
+    Name: "Scaffolding and painting.",
+    BuildingID: "222",
+    Status: "closed",
+    StartDate: "2016-12-12T00:00:00",
+    EndDate: "2016-12-14T00:00:00",
+    ContactPerson: "Joe Bloggs",
+    ProjectManager: "Sally Smith",
+    Contractor: "ABC Company",
+    Works: [{
+            TypeOfWork: "Scaffolfing",
+            Status: "done"
+        },
+        {
+            TypeOfWork: "Painting",
+            Status: "done"
+        }
+    ],
+
+    Comments: [{
+        Author: "Adele",
+        Text: "Hello."
+    }, {
+        Author: "John Doe",
+        Text: "Problem detected."
+    }]
+}];
 };
