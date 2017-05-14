271ecdd269ec15edcc8a49eea8609e86dea8f711
Christiandel Rabe <chris.rabe14@gmail.com>
2017-05-14 13:49:00 +1200

    Added some functions for adding, updating projects

--- datahandler.js
+++ datahandler.js
@@ -40,7 +40,109 @@ var updateBuilding = function (buildings, building) {
     }
 };
 
+// returns the project information
+var getProject = function () {
+    return project;
+};
+
+// sets the projects array in the data handler
+var setProjects = function (newProjects) {
+    projects = newProjects;
+};
+
+// Adds the new project into the array
+// Note : Assumes that the new project has a unique ID
+var addProject = function (newProject) {
+    if (newProject != null) {
+        projects.push(newProject);
+    }
+};
+
+// Updates the existing project into the array
+// Note: Assumes that the new project exists in the projects array
+var updateProject = function (newProject) {
+    if (newProject != null) {
+        for (i = 0; i < projects.length; i++) {
+            var item = projects[i];
+            if (item.ProjectID == newProject.ProjectID) {
+                projects[i] = newProject;
+                break;
+            }
+        }
+    }
+};
+
+// Construction Functions
+
+// Creates a new building object with empty fields.
+var createBuilding = function (buildings) {
+    // Create a unique id for the building object
+    var id = createID(buildings, false);
+    // Create an empty building
+    return { ID: id, Owner: "", Address: "", BuildingType: "", ConstructionDate: "" };
+};
+
+var createProject = function (buildingID) {
+    // create a unique id for the new project object
+    var id = createID(projects, true);
+    // Create an empty project
+    return {
+        ProjectID: id,
+        Name: "",
+        BuildingID: buildingID,
+        Status: "on-going",
+        StartDate: "",
+        EndDate: "",
+        ContactPerson: "",
+        ProjectManager: "",
+        Contractor: "",
+        Works: [],
+        Comments: []
+    };
+};
+
 // Helper Methods
+
+// Retrieves the id of the last element in the array
+var getLastID = function (array, isProject) {
+    var lastID = 0;
+    if (isProject) {
+        // sort the array from lowest to highest number
+        arrays.sort(function (a, b) { return a.ProjectID - b.ProjectID });
+        // retrieve the id of the last element of the array
+        lastID = parseInt(array[arrays.length - 1].ProjectID);
+    } else {
+        // Sort the array from lowest to highest number
+        array.sort(function (a, b) { return a.ID - b.ID });
+        // retrieve the id of the last element in the array
+        lastID = parseInt(array[array.length - 1].ID);
+    }
+    return lastID; // return the id
+};
+
+// If there are items in the array, 
+// then it increases the id of the last element in the array by one, otherwise returns zero
+var createID = function (array, isProject) {
+    if (array == null) {
+        return 0;
+    }
+    var newID = 0;
+    if (array.length > 0) {
+        // Retrieve the id of the last element
+        var lastID = getLastID(array, isProject);
+        newID = lastID + 1; // increase id of last element by one
+    }
+    return newID;
+};
+
+// Retrieves the id of the last element in the array
+var getLastID = function (array) {
+    // Sort the array from lowest to highest number
+    array.sort(function (a, b) { return a.ID - b.ID });
+    // retrieve the id of the last element in the array
+    var lastID = parseInt(array[array.length - 1].ID);
+    return lastID; // return the id
+};
 
 // We use this in order to avoid changing the list containing actual values on the server
 // Creates a copy of the given object - would have the same values but different memory address
