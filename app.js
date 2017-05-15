cd9ebefdca805cace8394ee20035f8676d826da6
Christiandel Rabe <chris.rabe14@gmail.com>
2017-05-15 12:45:15 +1200

    made some comment changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.controller('MainCtrl', function ($scope, $http) {
   $scope.updateBuilding = function () {
     var newBuilding = $scope.building;
     updateBuilding($scope.buildings, newBuilding);
-    // NOTE: Post the new building information to the server (Next Sprint)
+    // [TODO] Post the new building information to the server (Next Sprint)
   };
 
   // Navigation functions of scope
